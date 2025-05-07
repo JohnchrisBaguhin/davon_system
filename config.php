4b508245f77f17538d6b2a7b139c42eb36bde85f
Johnchris Baguhin <johnchris.baguhin@bisu.edu.ph>
2025-05-07 22:34:35 +0800

    fdfd

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
-// Database connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "crms";
+// Remote Database connection
+$servername = "192.185.48.158";
+$username = "bisublar_k9";
+$password = "k9Registration@2025";
+$dbname = "bisublar_k9";
 
+// Simple connection without SSL
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+?>
